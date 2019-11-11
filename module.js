6d3c4d9355067bbe9873b4f30e98aea37e68df6a
EPMA_Castaing <bruder.kraft225@gmail.com>
2019-11-11 13:32:44 -0600

    fix(module) export jeolimage and pfeimage

--- module.js
+++ module.js
@@ -1,8 +1,10 @@
 module.exports = {
-	NodeCanvas: require('./canvas/nodecanvasmodule'),
-	CanvasRoot: require('./canvas/canvasroot'),
-	constants: require('./constants'),
-	calculations: require('./calculations'),
-	PointShoot: require('./pointshoot'),
-	ExtractedMap: require('./extractedmap')
+	NodeCanvas: require('./canvas/nodecanvasmodule.js'),
+	CanvasRoot: require('./canvas/canvasroot.js'),
+	constants: require('./constants.json'),
+	calculations: require('./calculations.js'),
+	PointShoot: require('./pointshoot.js'),
+	ExtractedMap: require('./extractedmap.js'),
+	JeolImage: require('./jeolimage.js'),
+	PFEImage: require('./pfeimage.js')
 };
