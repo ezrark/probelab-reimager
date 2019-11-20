c33637d6213a8932bb9381f95c7b46422ed99d34
EPMA_Castaing <bruder.kraft225@gmail.com>
2019-11-19 18:27:40 -0600

    fix(inputstructure) clean resolve for better parse feat(inputstructure) began parser

--- models/directory.js
+++ models/directory.js
@@ -8,54 +8,66 @@ const Layer = require('../files/layer.js');
 const InputStructure = require('../inputstructure.js');
 
 module.exports = class Directory {
-    constructor(uri, reimager, inputStructure=new InputStructure(constants.inputStructures)) {
-        this.data = {
-            uri,
-            reimager,
-            files: new Map(),
-            subDirs: new Map(),
-            inputStructure
-        };
-    }
+	constructor(uri, reimager, inputStructure = new InputStructure(constants.inputStructures)) {
+		this.data = {
+			uri,
+			reimager,
+			files: new Map(),
+			subDirs: new Map(),
+			inputStructure
+		};
+	}
 
-    getUri() {
-        return this.data.uri;
-    }
+	getUri() {
+		return this.data.uri;
+	}
 
-    getFiles() {
-        return this.data.files;
-    }
+	getFiles() {
+		return this.data.files;
+	}
 
-    getImages() {
-        return this.data.images;
-    }
+	getImages() {
+		return this.data.images;
+	}
 
-    getAllImages() {
-        return Array.from(this.getSubDirectories().values()).flatMap(dir =>
-            dir.getAllImages()
-        );
-    }
+	getAllImages() {
+		return Array.from(this.getSubDirectories().values()).flatMap(dir =>
+			dir.getAllImages()
+		);
+	}
 
-    getSubDirectories() {
-        return this.data.subDirs;
-    }
+	getSubDirectories() {
+		return this.data.subDirs;
+	}
 
-    async refresh() {
-        const { files, dirs } = await fs.readdir(this.getUri(), {
-            withFileTypes: true
-        }).reduce((data, file) => {
-            if (file.isFile())
-                data.files.push(file);
-            else if (file.isDirectory())
-                data.dirs.push(file);
-            return data;
-        }, { files: [], dirs: [] });
+	getSubDirectory(name) {
+		return this.data.subDirs.get(name);
+	}
 
-        this.data.subDirs = new Map(dirs.map(dir => [
-            dir.name,
-            new Directory(`${this.data.uri}/${dir.name}`, this.data.reimager)
-        ]));
+	getFile(name) {
+		return this.data.files.get(name);
+	}
 
-        this.data.files = await this.data.inputStructure.process(files, this.data.subDirs);
-    }
+	getImage(name) {
+		return this.data.images.get(name);
+	}
+
+	async refresh() {
+		const {files, dirs} = await fs.readdir(this.getUri(), {
+			withFileTypes: true
+		}).reduce((data, file) => {
+			if (file.isFile())
+				data.files.push(file);
+			else if (file.isDirectory())
+				data.dirs.push(file);
+			return data;
+		}, {files: [], dirs: []});
+
+		this.data.subDirs = new Map(dirs.map(dir => [
+			dir.name,
+			new Directory(`${this.data.uri}/${dir.name}`, this.data.reimager)
+		]));
+
+		this.data.files = await this.data.inputStructure.process(files, this.data.subDirs);
+	}
 };
