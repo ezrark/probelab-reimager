e0d4f323a458700010df4221a4604b26aa719129
bob620 <bruder.kraft225@gmail.com>
2019-11-26 04:11:25 -0600

    problems

--- inputstructure.js
+++ inputstructure.js
@@ -34,7 +34,7 @@ class FileSet {
 		return this.files.get(type);
 	}
 
-	findFile(name, type=undefined, operation=undefined) {
+	findFile(name, type = undefined, operation = undefined) {
 		if (type) {
 			const easy = this.files.get(type).get(name);
 			if (easy)
@@ -70,19 +70,30 @@ class FileSet {
 
 module.exports = class InputStructure {
 	constructor(structure) {
-		this.types = new Map();
-		this.baseTypes = [];
-		this.needTypes = [];
-		this.useTypes = [];
-		this.useFakeTypes = [];
+		this.types = [];
+		this.baseTypes = new Map();
+		this.needTypes = new Map();
+		this.useTypes = new Map();
+		this.useFakeTypes = new Map();
+		this.startExt = new Map();
+		this.endExt = new Map();
+		this.equalExt = new Map();
+
+		let useTypes = [];
 
 		for (const {type, file, req} of structure) {
-			this.types.set(type, require(__dirname + file));
+			const module = require(__dirname + file);
+			let typeStruct = {
+				type,
+				module,
+				needs: 1,
+				uses: []
+			};
 
-			for (const {ext, needs, uses} of req) {
-				if (ext === undefined)
-					this.useFakeTypes.push({
-						type,
+			for (const {ext: baseExt, needs, uses} of req) {
+				if (baseExt === undefined) {
+					this.useFakeTypes.set(type, {
+						module,
 						needs,
 						uses: uses.map(str => {
 							const [type, filename] = str.split(':');
@@ -90,78 +101,140 @@ module.exports = class InputStructure {
 								type,
 								filename
 							};
-						}), resolvesIn: Infinity
+						})
 					});
-				else {
-					let parse = undefined;
-					if (ext.startsWith('*'))
-						parse = 'endsWith';
-					else if (ext.endsWith('*'))
-						parse = 'startsWith';
+				} else {
+					const ext = baseExt.replace(/\*/, '').toLowerCase();
 
-					if (needs && needs.length > 0) {
-						if (uses && uses.length > 0)
-							this.useTypes.push({
-								parse, ext: ext.replace(/\*/, ''), type, needs, uses: uses.map(str => {
-									const [type, filename] = str.split(':');
-									return {
-										type,
-										filename
-									};
-								})
-							});
+					if (baseExt.startsWith('*')) {
+						const exts = this.endExt.get(ext[ext.length - 1]);
+						if (exts) {
+							const types = exts.get(ext);
+							if (types)
+								types.push(type);
+							else
+								exts.set(ext, [type]);
+						} else
+							this.endExt.set(ext[ext.length - 1], new Map([[ext, [type]]]));
+					} else if (baseExt.endsWith('*')) {
+						const exts = this.startExt.get(ext[0]);
+						if (exts) {
+							const types = exts.get(ext);
+							if (types)
+								types.push(type);
+							else
+								exts.set(ext, [type]);
+						} else
+							this.startExt.set(ext[0], new Map([[ext, [type]]]));
+					} else {
+						const types = this.startExt.get(ext);
+						if (types)
+							types.push(type);
 						else
-							this.needTypes.push({parse, ext: ext.replace(/\*/, ''), type, needs});
-					} else if (uses && uses.length > 0)
-						this.useTypes.push({
-							parse, ext: ext.replace(/\*/, ''), type, uses: uses.map(str => {
-								const [type, filename] = str.split(':');
-								return {
-									type,
-									filename
-								};
-							})
-						});
+							this.equalExt.set(ext, type);
+					}
+
+					const structure = {
+						needs,
+						uses: uses ? uses.map(str => {
+							const [type, filename] = str.split(':');
+							return {
+								type,
+								filename
+							};
+						}) : undefined
+					};
+
+					if (uses && uses.length > 0)
+						this.useTypes.set(type, structure);
+					else if (needs && needs.length > 0)
+						this.needTypes.set(type, structure);
 					else
-						this.baseTypes.push({parse, ext: ext.replace(/\*/, ''), type});
+						this.baseTypes.set(type, structure);
+
+					if (needs)
+						typeStruct.needs = needs.length + 1;
+
+					if (structure.uses) {
+						typeStruct.uses = structure.uses.map(({type}) => type);
+
+						useTypes.push(typeStruct);
+					} else
+						this.types.push(typeStruct);
 				}
 			}
 		}
 
-		this.needTypes.sort(({needs: needs1}, {needs: needs2}) => needs1.length - needs2.length);
+		this.types.sort(({needs: needs1}, {needs: needs2}) => needs1 - needs2);
+		const resolved = new Map(this.types.map(({type, needs}) => [type, needs]));
+		const unresolvedUses = new Map(useTypes.map(({type, needs, uses}) => [type, {needs, uses: uses ? uses : []}]));
 
-		let resolved = new Map(this.baseTypes.concat(this.needTypes).map(e => [e.type, 1]));
-		let useTypes = new Map(this.useTypes.map(e => [e.type, e.uses.map(({type}) => type)]));
 		const resolve = (lastType, type) => {
 			if (resolved.has(type))
 				return resolved.get(type);
-			else if (!useTypes.has(type))
+			else if (!unresolvedUses.has(type))
 				throw `Unknown type ${type.type} required by ${lastType}`;
-			useTypes.get(type).map(resolve.bind(this, type));
+			const unresolved = unresolvedUses.get(type);
+			return unresolved.needs.map(resolve.bind(this, type)) + unresolved.uses.map(resolve.bind(this, type));
 		};
 
-		this.useTypes.sort(({type: type1, uses: uses1}, {type: type2, uses: uses2}) => {
+		this.types.concat(useTypes.sort(({type: type1, needs: needs1, uses: uses1}, {type: type2, needs: needs2, uses: uses2}) => {
 			let resolve1 = 0;
 			let resolve2 = 0;
 
 			if (!resolved.has(type1)) {
 				resolve1 = uses1.map(({type}) => type).reduce(resolve.bind(this, type1));
-				resolved.set(type1, resolve1);
+				resolved.set(type1, resolve1 + needs1);
 			} else
 				resolve1 = resolved.get(type1);
 
 			if (!resolved.has(type2)) {
 				resolve2 = uses2.map(({type}) => type).reduce(resolve.bind(this, type2));
-				resolved.set(type2, resolve2);
+				resolved.set(type2, resolve2 + needs2);
 			} else
 				resolve2 = resolved.get(type2);
 
-			return resolve1 + resolve2;
-		});
+			return (needs1 + resolve1) - (resolve2 + needs2);
+		}));
+
+		console.log();
+	}
+
+	equals(ext) {
+		const types = this.equalExt.get(ext);
+		return types ? types : [];
+	}
+
+	startsWith(ext) {
+		const exts = this.startExt.get(ext[0]);
+		let types = [];
+		if (exts)
+			types = exts.get(ext);
+		return types;
+	}
+
+	endsWith(ext) {
+		const exts = this.endExt.get(ext[ext.length - 1]);
+		let types = [];
+		if (exts)
+			types = exts.get(ext);
+		return types;
 	}
 
 	process(reimager, files, subDirs = new Map()) {
 		let resolved = new FileSet();
+		const types = files.reduce((files, file) => {
+			const ext = file.name.split('.').pop().toLowerCase();
+			for (type of this.equals(ext).concat(this.startExt(ext)).concat(this.endExt(ext))) {
+				const e = files.get(type);
+				if (e)
+					e.push(file);
+				else
+					files.set(type, [file]);
+			}
+
+			return files;
+		}, new Map());
 
 		const getRequiredFile = (name, type, str, prependName = true) => {
 			if (str) {
@@ -180,42 +253,49 @@ module.exports = class InputStructure {
 				return resolved.findFile(name, type);
 		};
 
-		for (const {parse, ext, type} of this.baseTypes) {
+		for (const {type} of this.baseTypes) {
 			const typeConstructor = this.types.get(type);
-			for (const {uri, stats} of files.filter(({name}) => (parse === undefined) ? (name.split('.').pop().toLowerCase() === ext) : (name.toLowerCase()[parse](ext))))
-				try {
-					resolved.resolve(type, new typeConstructor({uri, stats}, reimager));
-				} catch (err) {
-				}
+			const files = types.get(type);
+
+			if (files)
+				for (const {uri, stats} of files)
+					try {
+						resolved.resolve(type, new typeConstructor({uri, stats}, reimager));
+					} catch (err) {
+					}
 		}
 
-		for (const {parse, ext, type, needs} of this.needTypes) {
+		for (const {type, needs} of this.needTypes) {
 			const typeConstructor = this.types.get(type);
-			for (const {name, uri, stats} of files.filter(({name}) => (parse === undefined) ? (name.split('.').pop().toLowerCase() === ext) : (name.toLowerCase()[parse](ext))))
-				try {
-					resolved.resolve(type, new typeConstructor({uri, stats}, reimager,
-						needs.map(str => getRequiredFile(name, type, str))
-					));
-				} catch (err) {
-				}
+			const files = types.get(type);
+
+			if (files)
+				for (const {uri, stats} of files)
+					try {
+						resolved.resolve(type, new typeConstructor({uri, stats}, reimager,
+							needs.map(str => getRequiredFile(name, type, str))
+						));
+					} catch (err) {
+					}
 		}
 
-		for (const {parse, ext, type, needs = [], uses} of this.useTypes) {
+		for (const {type, needs, uses} of this.needTypes) {
 			const typeConstructor = this.types.get(type);
-			for (let {name, uri, stats} of files.filter(({name}) => (parse === undefined) ? (name.split('.').pop().toLowerCase() === ext) : (name.toLowerCase()[parse](ext))))
-				try {
-					resolved.resolve(type, new typeConstructor({uri, stats}, reimager,
-						needs.map(str => getRequiredFile(name, type, str)),
-						uses.map(({type, filename: str}) => getRequiredFile(name, type, str, false))
-					));
-				} catch (err) {
-				}
+			const files = types.get(type);
+
+			if (files)
+				for (const {uri, stats} of files)
+					try {
+						resolved.resolve(type, new typeConstructor({uri, stats}, reimager,
+							needs.map(str => getRequiredFile(name, type, str)),
+							uses.map(({type, filename: str}) => getRequiredFile(name, type, str, false))
+						));
+					} catch (err) {
+					}
 		}
 
-		for (const {type, uses} of this.useFakeTypes)
+		for (const {type, uses, module} of this.useFakeTypes)
 			try {
-				const typeConstructor = this.types.get(type);
-
 				if (uses.length === 1) {
 					const potential = resolved.getType(uses[0].type);
 					if (potential === undefined)
@@ -223,10 +303,10 @@ module.exports = class InputStructure {
 					if (uses[0].filename) {
 						for (const [name, data] of potential)
 							if (name === uses[0].filename)
-								resolved.resolve(type, new typeConstructor(data.getDirectoryUri(), reimager, [data]));
+								resolved.resolve(type, new module(data.getDirectoryUri(), reimager, [data]));
 					} else
 						for (const [, data] of potential)
-							resolved.resolve(type, new typeConstructor(data.getDirectoryUri(), reimager, [data]));
+							resolved.resolve(type, new module(data.getDirectoryUri(), reimager, [data]));
 				} else
 					for (const [, data] of resolved.getType(uses[0].type)) {
 						const {parse, name} = resolveName(uses[1].filename, [[uses[0].type, data.getName()]]);
@@ -241,7 +321,7 @@ module.exports = class InputStructure {
 								} else if (otherData.getName()[parse](name))
 									wantedFiles.push(otherData);
 
-						resolved.resolve(type, new typeConstructor(data.getDirectoryUri(), reimager, [data, wantedFiles]));
+						resolved.resolve(type, new module(data.getDirectoryUri(), reimager, [data, wantedFiles]));
 					}
 			} catch (err) {
 			}
